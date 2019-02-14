7ce8651aeabce53438942ff036dd7d91fc27a7cf
212615385 <benjamin.otte@ge.com>
2019-02-14 16:33:08 -0500

    Initial Commit

---
+++ Ellipses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ellipses
+//
+//  Created by Benjamin Otte on 2/14/19.
+//  Copyright © 2019 Benjamin Otte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
